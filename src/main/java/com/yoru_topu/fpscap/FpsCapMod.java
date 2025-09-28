ae48eef26ebe600b6e21d342a9c66d2e8903f349
Muhammad36861 <m.asharwaseem@gmail.com>
2025-09-28 16:19:47 +0500

    Update FpsCapMod.java

--- src/main/java/com/yoru_topu/fpscap/FpsCapMod.java
+++ src/main/java/com/yoru_topu/fpscap/FpsCapMod.java
@@ -1,1 +1,14 @@
+package com.yoru_topu.fpscap;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
+
+public class FpsCapMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        MinecraftClient client = MinecraftClient.getInstance();
+        client.options.getMaxFps().setValue(150);
+        System.out.println("[FPS Cap Mod by yoru_topu] FPS cap set to 150.");
+    }
+}
 
